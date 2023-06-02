3ff37e01c94f7726d9cee6749f85bb8789042be5
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-06-02 18:52:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
 provider "aws" {
   region     = "us-east-1"
 }
-
-  # The following null resource will print message "Hello World"
-resource "null_resource" "null_resource_simple" {
-# This trigger will only execute once when it detects the instance id of EC2 instance 
-  triggers = {
-    id = aws_instance.ec2_example.id    # to execute it every time replace - id = time()
-  }
-  provisioner "local-exec" {
-    command = "echo Hello World"
+  resource "aws_instance" "ec2_example" {
+  ami           = "ami-0715c1897453cabd1"
+  instance_type =  "t2.micro"
+  tags = {
+    Name = "Terraform EC2 "
   }
 }
-
+  # The following null resource will print message "Hello World"
+resource "null_resource" "null_resource_simple" {
+     triggers = {
+    id = aws_instance.ec2_example.id 
+  }
+  provisioner "local-exec" {
+    command = "echo Wagwan World"
+  }
+}
