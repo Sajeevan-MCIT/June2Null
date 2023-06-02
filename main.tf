e372308300db04ab72ff626f3757b715f72c03d5
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-06-02 19:16:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,13 @@ provider "aws" {
   }
 }
   # The following null resource will print message "Hello World"
-resource "null_resource" "null_resource_simple" {
-     triggers = {
-    id = aws_instance.ec2_example.id 
+ resource "null_resource" "null_resource_simple" {
+  triggers = {
+    id = aws_instance.ec2_example.id
   }
   provisioner "local-exec" {
-    command = "echo Wagwan World"
+    command = <<-EOT
+      touch hello-world.txt
+    EOT
   }
 }
