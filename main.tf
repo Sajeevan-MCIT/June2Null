a76c60d0b896811b0e1ad059d05d166268a0e6ff
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-06-02 18:34:42 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
 provider "aws" {
   region     = "us-east-1"
 }
-  resource "aws_instance" "ec2_example" {
-  ami           = "ami-0715c1897453cabd1"
-  instance_type =  "t2.micro"
-  tags = {
-    Name = "Terraform EC2 "
-  }
-}
+
   # The following null resource will print message "Hello World"
 resource "null_resource" "null_resource_simple" {
   provisioner "local-exec" {
-    command = "Wagwan World"
+    command = "echo Wagwan World"
   }
 }
 
